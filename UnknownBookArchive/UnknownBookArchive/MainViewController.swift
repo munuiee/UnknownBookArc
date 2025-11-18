2c015b5b8b99727641b06aa107870be82544d7aa
jyeee <munuiee@icloud.com>
2025-11-18 20:52:16 +0900

    Feat: 파일 생성

---
+++ UnknownBookArchive/UnknownBookArchive/MainViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    // 알라딘 API 키 불러오기
+    let APIKey = Bundle.main.infoDictionary?["APIKey"] as! String
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
