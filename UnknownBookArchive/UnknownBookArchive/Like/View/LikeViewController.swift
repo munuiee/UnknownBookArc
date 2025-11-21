f6cb885a85ba332e5ff37dbfcc7f9dd024bfe237
RiHA039 <riyaya0320@gmail.com>
2025-11-21 16:32:00 +0900

    [feat] #12 탭바 구현하기

---
+++ UnknownBookArchive/UnknownBookArchive/Like/View/LikeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LikeViewController.swift
+//  UnknownBookArchive
+//
+//  Created by 김리하 on 11/21/25.
+//
+
+import UIKit
+
+class LikeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "좋아요"
+    }
+}
