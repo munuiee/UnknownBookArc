86f90b730da8bc291555439c7e130ada4143173d
meowbyterh <riyaya0320@gmail.com>
2025-12-09 16:47:12 +0900

    [feat] #116 메인 화면 더보기 화면 디자인 수정

--- UnknownBookArchive/UnknownBookArchive/ReadingHome/View/MoreBookCell.swift
+++ UnknownBookArchive/UnknownBookArchive/ReadingHome/View/MoreBookCell.swift
@@ -8,10 +8,19 @@ final class MoreBookCell: UITableViewCell {
     static let identifier = "MoreBookCell"
     
     // MARK: - UI 요소
+    private let cardView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.primaryBlue50.cgColor
+        return view
+    }()
+    
     let thumbnailImageView = UIImageView()
     let titleLabel = UILabel()
     let authorLabel = UILabel()
-
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -19,9 +28,8 @@ final class MoreBookCell: UITableViewCell {
         setupLayout()
         
         selectionStyle = .none
-        contentView.backgroundColor = .white
-        contentView.layer.cornerRadius = 12
-        contentView.clipsToBounds = true
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
@@ -32,49 +40,62 @@ final class MoreBookCell: UITableViewCell {
     // MARK: UI 설정
     private func setupUI() {
         
+        // 카드뷰 안에 추가
+        contentView.addSubview(cardView)
+        
         // 썸네일
         thumbnailImageView.contentMode = .scaleAspectFill
         thumbnailImageView.clipsToBounds = true
-        thumbnailImageView.layer.cornerRadius = 6
-        thumbnailImageView.backgroundColor = .systemGray5
+        thumbnailImageView.layer.cornerRadius = 8
+        thumbnailImageView.layer.borderWidth = 1
+        thumbnailImageView.layer.borderColor = UIColor.primaryBlue50.cgColor
+        thumbnailImageView.backgroundColor = UIColor(white: 0.9, alpha: 1)
         
         // 제목
         titleLabel.font = UIFont.semiBoldFont(ofSize: 18)
         titleLabel.textColor = UIColor(red: 0.043, green: 0.078, blue: 0.176, alpha: 1.0)
+        titleLabel.numberOfLines = 1
         
         // 작가
         authorLabel.font = UIFont.mediumFont(ofSize: 14)
         authorLabel.textColor = UIColor(red: 0.506, green: 0.494, blue: 0.494, alpha: 1.0)
-
+        authorLabel.numberOfLines = 1
+        
+        [thumbnailImageView, titleLabel, authorLabel].forEach {
+            cardView.addSubview($0)
+        }
     }
     
     
     // MARK: 레이아웃
     private func setupLayout() {
-        contentView.addSubview(thumbnailImageView)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(authorLabel)
         
+        cardView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16))
+        }
+        
+        // 썸네일
         thumbnailImageView.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(20)
-            $0.centerY.equalToSuperview()
+            $0.top.equalTo(cardView).offset(8)
+            $0.leading.equalTo(cardView).offset(8)
             $0.width.equalTo(73)
             $0.height.equalTo(108)
+            $0.bottom.lessThanOrEqualTo(cardView).inset(8)
         }
         
+        // 제목 
         titleLabel.snp.makeConstraints {
-            $0.top.equalTo(thumbnailImageView.snp.top)
-            $0.leading.equalTo(thumbnailImageView.snp.trailing).offset(16)
-            $0.trailing.equalToSuperview().inset(20)   
+            $0.top.equalTo(thumbnailImageView.snp.top).offset(4)
+            $0.leading.equalTo(thumbnailImageView.snp.trailing).offset(8)
+            $0.trailing.equalTo(cardView).inset(8)
         }
         
+        // 작가
         authorLabel.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(6)
-            $0.leading.equalTo(titleLabel)
-            $0.trailing.equalTo(titleLabel)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(7)
+            $0.leading.trailing.equalTo(titleLabel)
         }
     }
-
     
     
     // MARK: 데이터 적용
@@ -83,6 +104,8 @@ final class MoreBookCell: UITableViewCell {
         if let data = book.coverImage,
            let image = UIImage(data: data) {
             thumbnailImageView.image = image
+        } else {
+            thumbnailImageView.image = nil
         }
         
         titleLabel.text = book.title
